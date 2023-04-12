4f17e8acda45d61836e970bfd82c48c3cdb003da
andry81 <andry@inbox.ru>
2023-04-12 21:33:14 +0300

    2023.04.12: * changed: tools/dump-externals.bat: script improvements

--- tools/dump-externals.bat
+++ tools/dump-externals.bat
@@ -1,11 +1,85 @@
 @echo off
 
+rem Description:
+rem   Script to collect the `vcstool` externals lists into a single list using
+rem   a categories list file as search directories.
+
+rem Usage:
+rem   dump-externals.bat <Flags> <Category-List-File> [<Prefix-Output-Dir> [<Suffix-Output-Dir>]]
+rem
+rem <Flags>:
+rem   -D
+rem     Script working directory.
+rem   -no-branches
+rem     Don't use and extract a `<branch>` subdirectory.
+rem
+rem <Category-List-File>:
+rem   Path to category list file to start search from as directories list.
+rem
+rem <Prefix-Output-Dir>:
+rem   Directory prefix for repo output local path.
+rem   The `src` by default.
+rem
+rem <Prefix-Input-Dir>:
+rem   Directory prefix for repo input local path.
+rem   The `_externals` by default.
+
+rem Examples:
+rem   >
+rem   dump-externals.bat -no-branches -D ../.. 3dparty\.categories.lst > .src.new
+
 setlocal
 
-rem Directory prefix for repo output local path.
-set "PREFIX_OUTPUT_DIR=%~1"
-rem Directory prefix for repo input local path.
-set "PREFIX_INPUT_DIR=%~2"
+rem script flags
+set "FLAG_WORKING_DIR="
+set FLAG_NO_BRANCHES=0
+
+:FLAGS_LOOP
+
+rem flags always at first
+set "FLAG=%~1"
+
+if defined FLAG ^
+if not "%FLAG:~0,1%" == "-" set "FLAG="
+
+if defined FLAG (
+  if "%FLAG%" == "-D" (
+    set "FLAG_WORKING_DIR=%~2"
+    shift
+  ) else if "%FLAG%" == "-no-branches" (
+    set FLAG_NO_BRANCHES=1
+  ) else (
+    echo.%?~nx0%: error: invalid flag: %FLAG%
+    exit /b -255
+  ) >&2
+
+  shift
+
+  rem read until no flags
+  goto FLAGS_LOOP
+)
+
+set "CATEGORIES_LIST_FILE=%~1"
+set "PREFIX_OUTPUT_DIR=%~2"
+set "PREFIX_INPUT_DIR=%~3"
+
+if not defined CATEGORIES_LIST_FILE (
+  echo.%~nx0: error: CATEGORIES_LIST_FILE is not defined.
+  exit /b 1
+) >&2
+
+if defined FLAG_WORKING_DIR for /F "eol= tokens=* delims=" %%i in ("%FLAG_WORKING_DIR%") do set "FLAG_WORKING_DIR=%%~fi"
+
+if "%CATEGORIES_LIST_FILE:~1,1%" == ":" (
+  for /F "eol= tokens=* delims=" %%i in ("%CATEGORIES_LIST_FILE%") do set "CATEGORIES_LIST_FILE=%%~fi"
+) else if defined FLAG_WORKING_DIR (
+  for /F "eol= tokens=* delims=" %%i in ("%FLAG_WORKING_DIR%\%CATEGORIES_LIST_FILE%") do set "CATEGORIES_LIST_FILE=%%~fi"
+) else for /F "eol= tokens=* delims=" %%i in ("%CATEGORIES_LIST_FILE%") do set "CATEGORIES_LIST_FILE=%%~fi"
+
+if not exist "%CATEGORIES_LIST_FILE%" (
+  echo.%~nx0: error: CATEGORIES_LIST_FILE does not exist: "%CATEGORIES_LIST_FILE%".
+  exit /b 255
+) >&2
 
 if not defined PREFIX_OUTPUT_DIR (
   set "PREFIX_OUTPUT_DIR=src"
@@ -25,10 +99,12 @@ if defined PREFIX_INPUT_DIR if "%PREFIX_INPUT_DIR:~-1%" == "/" set "PREFIX_INPUT
 
 :PREFIX_INPUT_DIR_END
 
-pushd "%~dp0..\%PREFIX_OUTPUT_DIR:/=\%" >nul && (
-  call :MAIN %%*
-  popd
-)
+if defined FLAG_WORKING_DIR (
+  pushd "%FLAG_WORKING_DIR%" >nul && (
+    call :MAIN %%*
+    popd
+  )
+) else call :MAIN %%*
 
 exit /b
 
@@ -38,7 +114,7 @@ echo.
 
 set IS_REPOSITORIES=0
 
-for /F "usebackq eol=# tokens=* delims=" %%i in ("%~dp0..\.categories.lst") do (
+for /F "usebackq eol=# tokens=* delims=" %%i in ("%CATEGORIES_LIST_FILE%") do (
   set "CATEGORY=%%i"
   call :PROCESS_CATEGORY
 )
@@ -63,6 +139,8 @@ for /F "usebackq eol=# tokens=* delims=" %%i in (`dir /A:D /B /O:N "%CATEGORY%"`
 exit /b 0
 
 :PROCESS_LIBDIR
+if %FLAG_NO_BRANCHES% NEQ 0 goto SKIP_PROCESS_BRANCHDIR
+
 for /F "usebackq eol=# tokens=* delims=" %%i in (`dir /A:D /B /O:N "%CATEGORY%\%LIBDIR%"`) do (
   set "BRANCHDIR=%%i"
   call :PROCESS_BRANCHDIR
@@ -84,6 +162,26 @@ set IS_REPOSITORIES=0
 set IS_NEXT_EXTERNAL=0
 
 for /F "usebackq eol=# tokens=* delims=" %%i in ("%CATEGORY%\%LIBDIR%\%BRANCHDIR%\.externals") do (
+  set "EXTERNALS_FILE_LINE=%%i"
+  call :PROCESS_EXTERNALS_FILE_LINE || exit /b
+)
+
+exit /b 0
+
+:SKIP_PROCESS_BRANCHDIR
+if not exist "%CATEGORY%\%LIBDIR%\.externals" exit /b 0
+
+if %IS_REPOSITORIES% NEQ 0 echo.
+rem safe echo
+setlocal ENABLEDELAYEDEXPANSION & for /F "eol= tokens=* delims=" %%i in ("!LIBDIR!") do endlocal & echo.  ## %%i/
+echo.
+
+rem echo "%CATEGORY%\%LIBDIR%\.externals"
+
+set IS_REPOSITORIES=0
+set IS_NEXT_EXTERNAL=0
+
+for /F "usebackq eol=# tokens=* delims=" %%i in ("%CATEGORY%\%LIBDIR%\.externals") do (
   set "EXTERNALS_FILE_LINE=%%i"
   call :PROCESS_EXTERNALS_FILE_LINE || exit /b
 )
